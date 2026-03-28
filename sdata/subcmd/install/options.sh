fd8436c0ca687625053e2bf348b35b9f944d58ab
Kioro404 <angelo61br@gmail.com>
2026-03-28 08:04:39 -0400

    add: subcomands for install argument

--- sdata/subcmd/install/options.sh
+++ sdata/subcmd/install/options.sh
@@ -6,27 +6,30 @@ printf "Syntax: $0 install [OPTIONS]...
 Idempotent installation for dotfiles.
 
 Options for install:
-  -h, --help                Print this help message and exit
-  -f, --force               (Dangerous) Force mode without any confirm
-  -F, --fisrtrun            Act like it is the first run
-  -c, --clean               Clean the build cache first
-      --skip-allgreeting    Skip the whole process greeting
-      --skip-alldeps        Skip the whole process installing dependency
-      --skip-allsetups      Skip the whole process setting up permissions/services etc
-      --skip-allfiles       Skip the whole process copying configuration files
-      --ignore-outdate      Ignore outdate checking for community supported \"dist-*\".
-  -s, --skip-sysupdate      Skip system package upgrade e.g. \"sudo pacman -Syu\"
-      --skip-plasmaintg     Skip installing plasma-browser-integration
-      --skip-backup         Skip backup conflicting files
-      --skip-quickshell     Skip installing the config for Quickshell
-      --skip-hyprland       Skip installing the config for Hyprland
-      --skip-fish           Skip installing the config for Fish
-      --skip-fontconfig     Skip installing the config for fontconfig
-      --skip-miscconf       Skip copying the dirs and files to \".configs\" except for
-                            Quickshell, Fish and Hyprland
-      --core                Alias of --skip-{plasmaintg,fish,miscconf,fontconfig}
-      --fontset <set>       Use a set of pre-defined font and config (currently only fontconfig).
-                            Possible values of <set>: $(ls -A ${REPO_ROOT}/dots-extra/fontsets)
+  -h, --help                     Print this help message and exit
+  -D, --deps                     Install dependencies only
+  -S, --setups                   Setup permissions/services
+  -F, --files                    Copy config files only
+  -f, --force                    (Dangerous) Force mode without any confirm
+  -c, --clean                    Clean the build cache first
+      --fisrtrun                 Act like it is the first run
+  -s, --skip-sysupdate           Skip system package upgrade e.g. \"sudo pacman -Syu\"
+      --skip-allgreeting         Skip the whole process greeting
+      --skip-alldeps             Skip the whole process installing dependency
+      --skip-allsetups           Skip the whole process setting up permissions/services etc
+      --skip-allfiles            Skip the whole process copying configuration files
+      --ignore-outdate           Ignore outdate checking for community supported \"dist-*\".
+      --skip-plasmaintg          Skip installing plasma-browser-integration
+      --skip-backup              Skip backup conflicting files
+      --skip-quickshell          Skip installing the config for Quickshell
+      --skip-hyprland            Skip installing the config for Hyprland
+      --skip-fish                Skip installing the config for Fish
+      --skip-fontconfig          Skip installing the config for fontconfig
+      --skip-miscconf            Skip copying the dirs and files to \".configs\" except for
+                                 Quickshell, Fish and Hyprland
+      --core                     Alias of --skip-{plasmaintg,fish,miscconf,fontconfig}
+      --fontset <set>            Use a set of pre-defined font and config (currently only fontconfig).
+                                 Possible values of <set>: $(ls -A ${REPO_ROOT}/dots-extra/fontsets)
 ${STY_CYAN}
 New features (experimental):
       --exp-files             Use yaml-based config for the third step copying files.
@@ -45,11 +48,13 @@ cleancache(){
 
 # `man getopt` to see more
 para=$(getopt \
-  -o hfFk:cs \
-  -l help,force,firstrun,fontset:,clean,skip-allgreeting,skip-alldeps,skip-allsetups,skip-allfiles,ignore-outdate,skip-sysupdate,skip-plasmaintg,skip-backup,skip-quickshell,skip-fish,skip-hyprland,skip-fontconfig,skip-miscconf,core,exp-files,via-nix \
+  -o hDSFk:fc \
+  -l deps,files,setups,help,force,firstrun,fontset:,clean,skip-allgreeting,skip-alldeps,skip-allsetups,skip-allfiles,ignore-outdate,skip-sysupdate,skip-plasmaintg,skip-backup,skip-quickshell,skip-fish,skip-hyprland,skip-fontconfig,skip-miscconf,core,exp-files,via-nix \
   -n "$0" -- "$@")
 [ $? != 0 ] && echo "$0: Error when getopt, please recheck parameters." && exit 1
+
 #####################################################################################
+
 ## getopt Phase 1
 # ignore parameter's order, execute options below first
 eval set -- "$para"
@@ -61,22 +66,29 @@ while true ; do
     *) shift ;;
   esac
 done
+
 #####################################################################################
+
 ## getopt Phase 2
 
 eval set -- "$para"
+
 while true ; do
   case "$1" in
+    ## Subcommands (short flags, before options)
+    -D|--deps) SUB_SUBCMD="deps"; shift;;
+    -S|--setups) SUB_SUBCMD="setups"; shift;;
+    -F|--files) SUB_SUBCMD="files"; shift;;
     ## Already processed in phase 1, but not exited
     -c|--clean) shift;;
     ## Ones without parameter
     -f|--force) ask=false;shift;;
-    -F|--firstrun) INSTALL_FIRSTRUN=true;shift;;
+    --firstrun) INSTALL_FIRSTRUN=true;shift;;
+    -s|--skip-sysupdate) SKIP_SYSUPDATE=true;shift;;
     --skip-allgreeting) SKIP_ALLGREETING=true;shift;;
     --skip-alldeps) SKIP_ALLDEPS=true;shift;;
     --skip-allsetups) SKIP_ALLSETUPS=true;shift;;
     --skip-allfiles) SKIP_ALLFILES=true;shift;;
-    -s|--skip-sysupdate) SKIP_SYSUPDATE=true;shift;;
     --ignore-outdate) IGNORE_OUTDATE_CHECK=true;shift;;
     --skip-plasmaintg) SKIP_PLASMAINTG=true;shift;;
     --skip-backup) SKIP_BACKUP=true;shift;;
